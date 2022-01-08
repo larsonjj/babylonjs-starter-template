d5358ba9dfebf5d7dde002f8c8099db3ccd239fe
Jake Larson <jake.j.larson@gmail.com>
2022-01-08 16:09:40 -0600

    Installed linting and formatting tools

--- src/gui.ts
+++ src/gui.ts
@@ -1,11 +1,16 @@
-﻿import {AbstractMesh} from "babylonjs";
-import {AdvancedDynamicTexture, Rectangle, Control, TextBlock} from "babylonjs-gui";
+﻿import { AbstractMesh } from 'babylonjs';
+import {
+  AdvancedDynamicTexture,
+  Control,
+  Rectangle,
+  TextBlock,
+} from 'babylonjs-gui';
 
 let advancedTexture: AdvancedDynamicTexture;
 
 function init(): void {
   if (!advancedTexture) {
-    advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("ui1");
+    advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI('ui1');
   }
 }
 
@@ -13,21 +18,21 @@ export function addLabelToMesh(mesh: AbstractMesh): void {
   if (!advancedTexture) {
     init();
   }
-  let label: Rectangle = new Rectangle("label for " + mesh.name);
-  label.background = "black";
-  label.height = "30px";
+  const label: Rectangle = new Rectangle('label for ' + mesh.name);
+  label.background = 'black';
+  label.height = '30px';
   label.alpha = 0.5;
-  label.width = "100px";
+  label.width = '100px';
   label.cornerRadius = 20;
   label.thickness = 1;
   label.linkOffsetY = 30;
-  label.top = "10%";
+  label.top = '10%';
   label.zIndex = 5;
   label.verticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
   advancedTexture.addControl(label);
 
   const text1: TextBlock = new TextBlock();
   text1.text = mesh.name;
-  text1.color = "white";
+  text1.color = 'white';
   label.addControl(text1);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,43 @@
-import './style.css'
+import './style.css';
 
-import {Engine, Scene, HemisphericLight, ArcRotateCamera, Vector3, MeshBuilder, Mesh} from "babylonjs";
-import {addLabelToMesh} from "./gui";
+import {
+  ArcRotateCamera,
+  Engine,
+  HemisphericLight,
+  Mesh,
+  MeshBuilder,
+  Scene,
+  Vector3,
+} from 'babylonjs';
+import { addLabelToMesh } from './gui';
 
-const canvas: any = document.getElementById("renderCanvas");
+const canvas: HTMLCanvasElement = document.getElementById(
+  'renderCanvas'
+) as HTMLCanvasElement;
 const engine: Engine = new Engine(canvas, true);
 
 function createScene(): Scene {
   const scene: Scene = new Scene(engine);
 
-  const camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+  const camera: ArcRotateCamera = new ArcRotateCamera(
+    'Camera',
+    Math.PI / 2,
+    Math.PI / 2,
+    2,
+    Vector3.Zero(),
+    scene
+  );
   camera.attachControl(canvas, true);
 
-  new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+  new HemisphericLight('light1', new Vector3(1, 1, 0), scene);
 
-  const sphere: Mesh = MeshBuilder.CreateSphere("sphere", {diameter: 1}, scene);
+  const sphere: Mesh = MeshBuilder.CreateSphere(
+    'sphere',
+    { diameter: 1 },
+    scene
+  );
 
   addLabelToMesh(sphere);
-
 
   return scene;
 }
